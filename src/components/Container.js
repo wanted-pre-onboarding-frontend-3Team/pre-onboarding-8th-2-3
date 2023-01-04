6541f2be1ee15fe633b2b485747f620a82e41b89
callmejeje <choije401@naver.com>
2023-01-04 21:57:06 +0900

    Feat: issues 리스트 통신 및 상태 저장

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
 import { issueCards } from 'constants/common';
 import Cards from './Cards';
 import styles from './Container.module.scss';
+import { getIssues } from 'models/issue';
+import { useRecoilState } from 'recoil';
+import { issues } from 'states/store';
 
 const Container = () => {
+  const [list, setList] = useRecoilState(issues);
+
+  useEffect(() => {
+    getIssues()
+      .then((issues) => {
+        setList(initializeIssues(issues));
+      })
+      .catch((error) => console.log(error));
+  }, [setList]);
+
+  const initializeIssues = (issues) => {
+    const newList = { todo: [], doing: [], done: [] };
+    for (const issue of issues) {
+      newList[issue.state].push(issue);
+    }
+    return newList;
+  };
+
   return (
     <div className={styles.container}>
       {issueCards.map((card) => (
-        <Cards key={card} title={card} />
+        <Cards key={card} title={card} list={list[card.toLowerCase().replace(/ /g, '')]} />
       ))}
     </div>
   );
