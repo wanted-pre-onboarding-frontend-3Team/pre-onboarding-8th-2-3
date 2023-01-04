dddc041ddd7b786e7065b9f60dde0b73ecf05ab2
callmejeje <choije401@naver.com>
2023-01-05 05:51:36 +0900

    Feat: 이슈 추가 모달

--- src/components/modal/AddModal.js
+++ src/components/modal/AddModal.js
@@ -1,7 +1,12 @@
+import IssueForm from 'components/IssueForm';
 import styles from './AddModal.module.scss';
 
 const AddModal = () => {
-  return <div className={styles.container}>AddModal</div>;
+  return (
+    <div className={styles.container}>
+      <IssueForm />
+    </div>
+  );
 };
 
 export default AddModal;
