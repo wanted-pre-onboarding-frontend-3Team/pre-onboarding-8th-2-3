b38c26e7d08a73ebc54596426366fd4076e6296d
callmejeje <choije401@naver.com>
2023-01-05 05:53:29 +0900

    Feat: 이슈 수정, 추가, 개별 이슈, 담당자 목록 함수 구현

--- src/models/issue.js
+++ src/models/issue.js
@@ -1,14 +1,29 @@
 import request from './base';
 
-export const getIssues = () => {
+export const getIssueList = () => {
   return request({ url: `/issues` });
 };
 
-// export const postIssue = () => {
-//   request({
-//     method: 'POST',
-//     url: '/issues',
-//     data: {
-//     },
-//   });
-// };
+export const getIssue = (id) => {
+  return request({ url: `/issues/${id}` });
+};
+
+export const editIssue = (id, data) => {
+  return request({
+    method: 'PUT',
+    url: `/issues/${id}`,
+    data,
+  });
+};
+
+export const postIssue = (data) => {
+  return request({
+    method: 'POST',
+    url: '/issues',
+    data,
+  });
+};
+
+export const getManagerList = () => {
+  return request({ url: `/managers` });
+};
