f55de42934acc17e89dfa11932e32d85b82b06d6
Woong <rlqmatk@naver.com>
2023-01-06 00:07:16 +0900

    Feat: issueSelector 추가해 api 요청 시 data 관리

--- src/components/modal/ModalForm.js
+++ src/components/modal/ModalForm.js
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { selectManager } from 'states/selectManagerState';
 import ModalContent from './form/ModalContent';
 import ModalEndDate from './form/ModalEndDate';
@@ -12,11 +12,13 @@ import { deleteIssue, editIssue, getManages, postIssue } from 'models/issue';
 import { v1 } from 'uuid';
 import ModalState from './form/ModalState';
 import { orderHandler } from 'utils/orderFilter';
+import { issuesSelector } from 'states/issueState';
 
 const ModalForm = ({ issue, selectedIssue }) => {
   const [managers, setManagers] = useState([]);
 
   const selectedManager = useRecoilValue(selectManager);
+  const setIssue = useSetRecoilState(issuesSelector);
   const navigate = useNavigate();
 
   const titleRef = useRef(null);
@@ -43,11 +45,11 @@ const ModalForm = ({ issue, selectedIssue }) => {
     fetchManagerList();
   }, [fetchManagerList]);
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
 
     if (selectedIssue) {
-      editIssue(pathId, {
+      await editIssue(pathId, {
         id: selectedIssue.id,
         order: selectedIssue.order,
         title: titleRef.current.value,
@@ -55,9 +57,9 @@ const ModalForm = ({ issue, selectedIssue }) => {
         endDate: dateRef.current.value,
         state: stateRef.current.value,
         manager: selectedManager || selectedIssue.manager,
-      });
+      }).then((data) => setIssue(['PUT', data]));
     } else {
-      postIssue({
+      await postIssue({
         id: v1(),
         order: orderHandler(issue, stateRef.current.value),
         title: titleRef.current.value,
@@ -65,15 +67,15 @@ const ModalForm = ({ issue, selectedIssue }) => {
         endDate: dateRef.current.value,
         state: stateRef.current.value,
         manager: selectedManager,
-      });
+      }).then((data) => setIssue(['POST', data]));
     }
     navigate('/');
   };
 
   const cancelHandler = () => navigate('/');
 
-  const deleteHandler = () => {
-    deleteIssue(pathId);
+  const deleteHandler = async () => {
+    await deleteIssue(pathId).then(() => setIssue(['DELETE', pathId]));
     navigate('/');
   };
 
