8ab0fc89eb846702c763aef450d1358c97d8c259
sweesweett <sweesweet@naver.com>
2023-01-06 13:32:31 +0900

    Feat:dnd 구현

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,18 +4,42 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { convertPathTitle, sortIssueArray } from 'utils/sortIssueArray';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { issueState } from 'states/issueState';
 
 const Cards = ({ title, list, loading }) => {
   const navigate = useNavigate();
-
+  const [data, setData] = useRecoilState(issueState);
   const addCardHandler = useCallback(() => navigate(`/?modal=add&state=${convertPathTitle(title)}`), [navigate, title]);
 
   const filteredIssue = sortIssueArray(list, title);
 
+  const handleDrop = (e) => {
+    const itemId = e.dataTransfer.getData('itemId');
+    const from = e.dataTransfer.getData('listName').split('_')[0];
+    const to = e.target.tagName === 'LI' ? e.target.parentElement.id : e.target.id;
+    console.log(data);
+    const newData = [...data].map((el) => {
+      if (el.id === itemId) {
+        return { ...el, state: convertPathTitle(to.split('_')[0]) };
+      }
+      return { ...el };
+    });
+    const dd = { ...data.filter((el) => el.id === itemId)[0], state: convertPathTitle(to.split('_')[0]) };
+    setData(newData);
+  };
+
   return (
     <section className={styles.container}>
       <p>{title}</p>
-      <ul className={styles.content}>
+      <ul
+        id={`${title}_list`}
+        className={styles.content}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={handleDrop}
+      >
         {!loading &&
           filteredIssue.map((issue) => (
             <Content key={issue.id} title={issue.title} id={issue.id} pathState={title} manager={issue.manager} />
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,29 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { convertPathTitle } from 'utils/sortIssueArray';
 import styles from './Content.module.scss';
 
 const Content = ({ title, id, pathState, manager }) => {
   const navigate = useNavigate();
+  const [target, setTarget] = useState([0, 0]);
 
   const detailCardHandler = useCallback(
     () => navigate(`?modal=detail&state=${convertPathTitle(pathState)}&id=${id}`),
     [navigate, id, pathState],
   );
+  const handleDragStart = (e, id) => {
+    e.dataTransfer.setData('itemId', id);
+    e.dataTransfer.setData('listName', e.target.parentElement.id);
+  };
 
   return (
-    <li className={styles.content} role="presentation" onClick={detailCardHandler} draggable>
+    <li
+      className={styles.content}
+      role="presentation"
+      onClick={detailCardHandler}
+      onDragStart={(e) => handleDragStart(e, id)}
+      draggable
+    >
       <span>{title}</span>
       {manager && <span className={styles.profile}> {manager.slice(0, 1)}</span>}
     </li>
